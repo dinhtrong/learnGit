5038ca1bf7240961d12786f81278ef9529b89e96
TrongHoang <tronghoang@tamdongtam.vn>
2018-11-11 10:10:30 +0700

    init Base Project

---
+++ src/screen/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import {  } from 'react-native';
+export default class Home extends Component {
+    render() {
+        return (
+           null 
+        );
+    }
+}
