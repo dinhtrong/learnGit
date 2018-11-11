2d13ae250ee0a6e90894d0f5c8789bc153e7dd75
Thang Hoang Ngoc <thanghoangngoc@mmms-MacBook-Pro.local>
2018-11-11 11:10:54 +0700

    edit pull dev3

--- src/screen/asdsadsdasdas.js
+++ src/screen/asdsadsdasdas.js
@@ -11,7 +11,7 @@ export default class asdsa extends Component {
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <Text> thang </Text>
       </View>
     );
   }
